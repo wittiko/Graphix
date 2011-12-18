b9f6c6cb886c2be35d09edc31bcb3a0cc2f47a95
Gregor Messner <gm@gregor-00.hec-tech.intern>
2011-12-19 00:24:05 +0100

    some tests with maven

---
+++ src/main/java/at/htlv/messner/graphix/HelloWorld.java
@@ -0,0 +1,16 @@
+package at.htlv.messner.graphix;
+
+public class HelloWorld 
+{
+	public HelloWorld()
+	{
+		printHelloWorld();
+	}
+	
+	public void printHelloWorld()
+	{
+		System.out.println( "Hello World!" );
+	}
+	
+
+}
