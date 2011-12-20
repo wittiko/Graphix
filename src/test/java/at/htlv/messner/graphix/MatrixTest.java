000359a2747ba74cdb5fe247bd835e8a47c25a3e
Gregor Messner <gm@gregor-00.hec-tech.intern>
2011-12-20 16:10:49 +0100

    Matrix tests

---
+++ src/test/java/at/htlv/messner/graphix/MatrixTest.java
@@ -0,0 +1,29 @@
+package at.htlv.messner.graphix;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.IsNull.notNullValue;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import at.htlv.messner.graphix.model.Matrix;
+
+public class MatrixTest 
+{
+	@Test
+	public void whenWeUseTheDefaultConstructorWeShouldGet8AsDimension()
+	{
+		Matrix m = new Matrix();
+		assertThat(m.getDimension(), is(8));
+	}
+	
+	@Test
+	public void whenWeSetTheDimensionWeShouldGetTheDimension()
+	{
+		Matrix m = new Matrix(5);
+		assertThat(m.getDimension(), is(5));
+	}
+
+}
